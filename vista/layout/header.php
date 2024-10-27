32cb9a73ae9991a8696d850113256d3125bd9aa8
EliasMiranda02 <9613266784az@gmail.com>
2024-10-26 20:07:38 -0600

    Add files via upload

---
+++ vista/layout/header.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Chiapas Viajero</title>
+    <link rel = "icon" type = "image/ico" href = "img/Icono.ico" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+    crossorigin="anonymous">
+    <link rel = "stylesheet" href = "css/home.css">
+    <style>
+        .carrusel-contain {
+            background-image: url('./img/home/Fondo.png');
+            background-size: cover; 
+            background-position: center; 
+            height: 573px; 
+            width: 100%; 
+        }
+    </style>
+</head>
+
+<body>
+    <!-- Encabezado -->
+    <header class="header">
+        <nav class="navbar">
+            <a href="#">SERVICIOS</a>
+            <a href="#">HISTORIAL</a>
+            <img src="img/Logo.png" alt="Logo" class="logo">
+            <a href="#">NOTIFICACIONES</a>
+            <a href="#">ACERCA DE</a>
+        </nav>
+        <img src="https://imgur.com/r1pSXR0" alt="Banner" class="banner">
+
+        <a href="#" class="reserva-button">
+            <span class="inner-button">¡Reserva Aquí!</span>
+        </a>
+
+        <a href="#" class="access-button">ACCESO</a>
+    </header><br><br>
+
+    
+ 
